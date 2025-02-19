f50ad5c851ff3872c992076e8432e0a56b5b46ae
yevhenii <yevheniisalo@gmail.com>
2025-02-18 23:17:30 -0700

    Main.py

--- database.py
+++ database.py
@@ -2,10 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Integer, String, Column, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # Получаем строку подключения из переменной окружения
 DATABASE_URL = os.getenv("DATABASE_URL")
